e8622debace9750449a96b7ed6d01f8f7e13084c
João Vitor Martins <joaoescmartins@gmail.com>
2026-02-22 03:46:29 -0300

    Implementação do Lombok nas classes NinjaModel e MissoesModel para abstracao de construtores

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -2,12 +2,18 @@ package dev.java10x.CadastroDeNinjas.Ninjas;
 
 import dev.java10x.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 //Entity transforma uma classe em uma entidade no banco de dados
 //JPA= Java Persistance API
 @Entity
 @Table (name = "tb_cadastro")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NinjaModel {
 
     @Id
@@ -16,41 +22,10 @@ public class NinjaModel {
     private String nome;
     private String email;
     private int idade;
+
     //@ManyToOne - um ninja tem uma única missao
     @ManyToOne
     @JoinColumn(name = "missoes_id") //Foreign Key = Chave Estrangeira
     private MissoesModel missoes;
 
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
