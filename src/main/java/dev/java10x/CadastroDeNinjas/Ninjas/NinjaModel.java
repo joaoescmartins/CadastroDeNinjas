328e648f1f231524a25ac2425913ee2aed9dbf17
João Vitor Martins <joaoescmartins@gmail.com>
2026-02-22 05:56:00 -0300

    Implementação NinjaRepository e conexão JPA

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -18,7 +18,7 @@ public class NinjaModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private long id;
     private String nome;
     @Column(unique = true)
     private String email;
